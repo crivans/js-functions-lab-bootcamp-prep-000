15ce009254af129bd8784482824b77cf5ac08fd4
Sorel Ivan <sorelidjomou@gmail.com>
2019-05-15 20:14:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    20describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-
-  describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
-
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+l('Happy Independence Day, you!')
     })
   })
 
